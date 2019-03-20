b95823a0c6a650a54776cdb43b59c8d80b725768
宋洪锋 <hong19970529@qq.com>
2019-03-20 11:28:12 +0800

    3.8商品分类管理进货管理初步

--- web-app/vue/src/views/nav4/comein.vue
+++ web-app/vue/src/views/nav4/comein.vue
@@ -1,14 +1,397 @@
 <template>
-    <section>page7...
-        </section>
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px" @submit.prevent="onSubmit" style="100%" v-loading="loading">
+        <el-form-item label="  供应商" prop="name">
+            <el-input v-model="form.name" style="width: 50%;"></el-input>
+        </el-form-item>
+        <el-form-item label="货物分类" prop="phone">
+            <el-input v-model="form.phone" style="width: 50%;"></el-input>
+        </el-form-item>
+        <el-form-item label="进货日期">
+            <el-input v-model="form.email" style="width: 50%;"></el-input>
+        </el-form-item>
+        <el-form-item label="付款金额">
+            <el-input v-model="form.qq" style="width: 50%;"></el-input>
+        </el-form-item>
+        <el-form-item label="付款状态">
+            <el-input v-model="form.qq" style="width: 50%;"></el-input>
+        </el-form-item>
+        <el-form-item label="付款日期">
+            <el-col :span="11">
+                <el-date-picker type="date" placeholder="选择日期" v-model="form.birth" style="width: 100%;"></el-date-picker>
+            </el-col>
+        </el-form-item>
+        <el-form-item label="备注">
+            <el-input type="textarea" v-model="form.remark"></el-input>
+        </el-form-item>
+        <el-form-item label="选择商品">
+            <el-select
+                    v-model="value10"
+                    multiple
+                    filterable
+                    :loading="gloading"
+                    @change="change()"
+                    placeholder="选择商品">
+                <el-option
+                        v-for="item in options5"
+                        :key="item.label"
+                        :label="item.label"
+                        :value="item.value"
+                        :disabled="item.disabled">
+                </el-option>
+            </el-select>
+            <el-input
+                    style="width: 150px"
+                    placeholder="输入内容查找商品"
+                    v-model="name"
+                    clearable>
+            </el-input>
+            <el-button type="primary" v-on:click="getGoods">搜索</el-button>
+        </el-form-item>
+        <el-form-item label="商品列表">
+            <el-table
+                    :data="goodsTable"
+                    border
+                    :summary-method="getSummaries"
+                    show-summary
+                    style="width: 100%">
+                <el-table-column
+                        prop="code"
+                        label="商品编号"
+                        width="180">
+                </el-table-column>
+                <el-table-column
+                        prop="name"
+                        label="产品名称">
+                </el-table-column>
+                <el-table-column
+                        prop="price"
+                        label="商品单价">
+                </el-table-column>
+                <el-table-column
+                        prop="unit"
+                        label="产品单位">
+                </el-table-column>
+                <el-table-column label="数量" width="250">
+                    <template slot-scope="scope">
+                        <el-input
+                                placeholder="请输入内容"
+                                v-model="scope.row.num"
+                                @change="addCli(scope.row)"
+                                clearable>
+                        </el-input>
+                    </template>
+                </el-table-column>
+                <el-table-column fixed="right"   prop="amount" label="小计">
+                </el-table-column>
+            </el-table>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit">立即创建</el-button>
+            <el-button @click="cancel">取消</el-button>
+        </el-form-item>
+    </el-form>
 </template>
 
 <script>
+    import util from '../../common/js/util'
     export default {
-        name: "comein"
+        data() {
+            return {
+                name:"",
+                uuuName:"",
+                disabled:true,
+                gloading:false,
+                uloading:false,
+                ruleForm: {
+                    modeTransport : "",//运输方式
+                    _Uid: "",//客户id
+                    leadTime: " ",//交货时间
+                    remark : "",//备注
+                    amount :0,//合计金额
+                    addr:"",//交货地址
+                    num9:"",//数量
+                    earnest:0,
+                },
+                formLoading:false,
+                rules: {
+                    _Uid: [
+                        { required: true, message: '请输入用户', trigger: 'blur' },
+                    ],
+                    region: [
+                        { required: true, message: '请选择活动区域', trigger: 'change' }
+                    ],
+                    date1: [
+                        { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+                    ],
+                    date2: [
+                        { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+                    ],
+                    type: [
+                        { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+                    ],
+                    resource: [
+                        { required: true, message: '请选择活动资源', trigger: 'change' }
+                    ],
+                    desc: [
+                        { required: true, message: '请填写活动形式', trigger: 'blur' }
+                    ]
+                },
+                users: [],
+                list: [],
+                loading: false,
+                options5: [{
+                    value: 'HTML',
+                    label: 'HTML'
+                }, {
+                    value: 'CSS',
+                    label: 'CSS'
+                }, {
+                    value: 'JavaScript',
+                    label: 'JavaScript'
+                }],
+                value10: [],
+                goodsTable:[],
+                form: {
+                    name: '',
+                    email : "",
+                    idCardNumber : "",
+                    phone : "",
+                    qq : " ",
+                    remark : "",
+                    sex:"",
+                    birth:"",
+                    addr:"",
+                },
+                rules:{
+                    name: [
+                        { required: true, message: '请输入姓名', trigger: 'blur' },
+                        { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' }
+                    ],
+                    email : "",
+                    idCardNumber : "",
+                    phone : [
+                        { required: true, message: '请输入电话', trigger: 'blur' },
+                        { min: 5, max: 15, message: '长度在 5 到 15 个字符', trigger: 'blur' }
+                    ],
+                    qq : " ",
+                    remark : "",
+                    sex:[
+                        { required: true, message: '请选择性别', trigger: 'change' }
+                    ],
+                    birth:"",
+                    addr:"",
+                },
+                loading:false,
+            }
+        },
+        methods: {
+            getSummaries(param) {
+                const { columns, data } = param;
+                const sums = [];
+                columns.forEach((column, index) => {
+                    if (index === 0) {
+                        sums[index] = '合计金额';
+                        return;
+                    }
+                    if (index === 1||index === 2||index === 3) {
+                        sums[index] = '  ';
+                        return;
+                    }
+
+                    const values = data.map(item => Number(item[column.property]));
+                    if (!values.every(value => isNaN(value))) {
+                        sums[index] = values.reduce((prev, curr) => {
+                            const value = Number(curr);
+                            if (!isNaN(value)) {
+                                return prev + curr;
+                            } else {
+                                return prev;
+                            }
+                        }, 0);
+                        sums[index]=sums[index].toFixed(3)+' 元';
+                    } else {
+                        sums[index] = 'N/A';
+                    }
+                });
+
+                return sums;
+            },
+            addCli(row){
+                row.amount= parseFloat((row.price*row.num).toFixed(3))
+                let total=0.0
+                this.goodsTable.forEach(it=>{
+                    total=parseFloat(it.amount+total)
+                })
+                this.ruleForm.amount=total
+            },
+            change(){
+                this.goodsTable=[]
+                this.value10.forEach(it=>{
+                    let g=JSON.parse(it)
+                    g.amount=0
+                    this.goodsTable.push(g)
+                })
+            },
+            getGoods(){
+                this.gloading=true
+                $.getJSON('api/product/getGoods',{name:this.name}).then(data=>{
+                    this.options5=[]
+                    data.list.forEach(item=>{
+                        let disabled
+                        if(item.state==0)
+                            disabled=false
+                        else
+                            disabled=true
+                        this.options5.push({value:JSON.stringify(item),label:item.name,disabled:disabled})
+                    })
+                    this.gloading=false
+                })
+            },
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        this.loading=true
+                        let form=JSON.parse(JSON.stringify(this.ruleForm))
+                        let detail=[]
+                        this.goodsTable.forEach(it=>{
+                            let a={
+                                _id:it._id,
+                                name:it.name,
+                                code:it.code,
+                                num:it.num,
+                                price:it.price,
+                            }
+                            detail.push(a)
+                        })
+                        form.detail=JSON.stringify(detail)
+                        form.leadTime = (!this.ruleForm.leadTime || this.ruleForm.leadTime == '') ? '' : util.formatDate.format(new Date(this.ruleForm.leadTime), 'yyyy-MM-dd');
+                        $.getJSON('api/order/creat',form).then(data=>{
+                            if(data.flag){
+                                this.$notify({
+                                    title: '成功',
+                                    message: data.remark,
+                                    type: 'success'
+                                });
+                                this.$refs['ruleForm'].resetFields();
+                            }else {
+                                this.$message({
+                                    message: data.remark,
+                                    type: 'error'
+                                });
+                            }
+                            this.loading=false
+                        })
+                    } else {
+                        this.$message({
+                            message:'请正确填写必要数据！',
+                            type: 'error'
+                        });
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            },
+            remoteMethod(query) {
+                this.loading=true
+                if (query !== '') {
+                    $.getJSON('api/customer/getUsers',{name:query}).then(data=>{
+                        if(data.flag){
+                            this.users=data.users
+                            if(this.users.length>=1){
+                                this.disabled=true
+                            }else {
+                                this.uuuName=query
+                                this.disabled=false
+                            }
+                        }else {
+                            this.$message({
+                                message: data.remark,
+                                type: 'error'
+                            });
+                        }
+                        this.loading=false
+                    })
+                } else {
+                    this.users = [];
+                    this.loading=false
+                }
+            },
+            creatUU(){
+                this.uloading=true
+                let form={
+                    name:this.uuuName,
+                    phone:this.uuuName,
+                    sex:"男",
+                }
+                $.getJSON('api/customer/creat',form).then(data=>{
+                    if(data.flag){
+                        this.$notify({
+                            title: '创建用户成功',
+                            message: data.remark,
+                            type: 'success'
+                        });
+                        this.disabled=true
+                    }else {
+                        this.$message({
+                            message: data.remark,
+                            type: 'error'
+                        });
+                    }
+                    this.uloading=false
+                    this.remoteMethod(this.uuuName)
+                })
+            },
+
+        mounted() {
+            this.getGoods()
+        },
+            onSubmit() {
+                this.$refs['form'].validate((valid) => {
+                    if (valid) {
+                        this.loading=true
+                        this.form.birth = (!this.form.birth || this.form.birth == '') ? '' : util.formatDate.format(new Date(this.form.birth), 'yyyy-MM-dd');
+                        $.getJSON('api/customer/creat',this.form).then(data=>{
+                            if(data.flag){
+                                this.$notify({
+                                    title: '成功',
+                                    message: data.remark,
+                                    type: 'success'
+                                });
+                                this.form= {
+                                    name: '',
+                                    email : "",
+                                    idCardNumber : "",
+                                    phone : "",
+                                    qq : " ",
+                                    remark : "",
+                                    sex:"",
+                                    birth:"",
+                                    addr:"",
+                                }
+                                this.$refs['form'].resetFields();
+                                this.loading=false
+                            }else {
+                                this.$message({
+                                    message: data.remark,
+                                    type: 'error'
+                                });
+                            }
+                        })
+                    } else {
+                        this.$message({
+                            message:'请填写必要数据！',
+                            type: 'error'
+                        });
+                        return false;
+                    }
+                });
+            },
+            cancel(){
+                this.$router.push({ path: '/table' });
+            }
+        }
     }
+
 </script>
-
-<style scoped>
-
-</style>
