e95e3cfca228b61edac15f65ab50647a47a432c9
宋洪锋 <hong19970529@qq.com>
2019-03-27 16:49:17 +0800

    4.6 供应商增删改查

--- grails-app/controllers/songHong/StockController.groovy
+++ grails-app/controllers/songHong/StockController.groovy
@@ -1,107 +1,128 @@
 package songHong
+import com.alibaba.fastjson.JSONObject
+class StockController extends BaseController{
 
-import static org.springframework.http.HttpStatus.*
-import grails.transaction.Transactional
-
-@Transactional(readOnly = true)
-class StockController {
-
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
-
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Stock.list(params), model:[stockCount: Stock.count()]
+    def creat={
+        def cus=Stock.newOne([
+                name:params.name,
+                email:params.email,
+                elephone:params.elephone,
+                remark :params.remark,
+                address:params.address,
+        ])
+        dataService.mongoDb.saveStock(cus)
+        render(js(true,"添加成功"))
     }
-
-    def show(Stock stock) {
-        respond stock
-    }
-
-    def create() {
-        respond new Stock(params)
-    }
-
-    @Transactional
-    def save(Stock stock) {
-        if (stock == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
-            return
-        }
-
-        if (stock.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond stock.errors, view:'create'
-            return
-        }
-
-        stock.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'stock.label', default: 'Stock'), stock.id])
-                redirect stock
+    def getCustomers={
+        def page=params.page
+        println("请求客户数据")
+        def form=[sort:[_id:-1]]
+        if(params.name){
+            def nu=params.name
+            try{
+                nu=Integer.parseInt(params.name)
+                form+=[elephone: [$regex:/^${nu.toString()}/]]
+                println("电话查找")
+            }catch(Exception e){
+                println("名字查找："+nu)
+                form+=[name: [$regex:/^${nu}/]]
             }
-            '*' { respond stock, [status: CREATED] }
+        }
+        def users=dataService.mongoDb.searchStock(form,page,20)
+        render(JSONObject.toJSONString([users:users.contentlist, num:users.allNum]))
+    }
+    def getUsers={
+        println("查找供应商数据"+params)
+        def form=[sort:[_id:-1]]
+        if(params.name){
+            def nu=params.name
+            try{
+                nu=Integer.parseInt(params.name)
+                form+=[elephone: [$regex:/^${nu.toString()}/]]
+                println("电话查找")
+            }catch(Exception e){
+                println("名字查找："+nu)
+                form+=[name: [$regex:/^${nu}/]]
+            }
+            def u=dataService.mongoDb.searchStock(form,1,40)
+            println("查找"+u)
+            def users=[]
+            u.contentlist.each{
+                users.add([
+                        value:JSONObject.toJSONString(it),
+                        label:it.name
+                ])
+            }
+            render(JSONObject.toJSONString([users:users,flag:true]))
         }
     }
-
-    def edit(Stock stock) {
-        respond stock
-    }
-
-    @Transactional
-    def update(Stock stock) {
-        if (stock == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
+    def edit={
+        println("修改用户数据")
+        def cu=dataService.mongoDb.findOneStock([_id:params._id])
+        if(!cu){
+            render(js(false,"此用户不存在！"))
             return
         }
-
-        if (stock.hasErrors()) {
-            transactionStatus.setRollbackOnly()
-            respond stock.errors, view:'edit'
+        if(cu._creatId==params._id||session.user.superUser){
+            def cus=[
+                    name:params.name,
+                    email:params.email,
+                    idCardNumber:params.idCardNumber,
+                    phone:params.phone,
+                    qq :params.qq,
+                    remark :params.remark,
+                    sex:params.sex,
+                    birth:params.birth,
+                    addr:params.addr,
+            ]
+            dataService.mongoDb.updateStock([_id:params._id],cus)
+            render(js(true,"修改成功"))
             return
         }
-
-        stock.save flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'stock.label', default: 'Stock'), stock.id])
-                redirect stock
-            }
-            '*'{ respond stock, [status: OK] }
-        }
+        render(js(false,"你不具有此权限，只有创建者或超级管理员可以删除"))
     }
-
-    @Transactional
-    def delete(Stock stock) {
-
-        if (stock == null) {
-            transactionStatus.setRollbackOnly()
-            notFound()
+    def delete={
+        def _id=params._id
+        def cu=dataService.mongoDb.findOneStock([_id:_id])
+        if(!cu){
+            render(js(false,"此用户不存在！"))
             return
         }
-
-        stock.delete flush:true
-
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'stock.label', default: 'Stock'), stock.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
+        if(cu._creatId==params._id||session.user.superUser){
+            dataService.mongoDb.delCustomer([_id:_id])
+            render(js(true,"删除成功！"))
+            return
         }
+        render(js(false,"只有创建者或超级管理员能删除！"))
     }
-
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'stock.label', default: 'Stock'), params.id])
-                redirect action: "index", method: "GET"
+    def deletes={
+        println(params)
+        def ids=params."ids[]"
+        def id=[]
+        if(ids instanceof String){
+            id.add(ids)
+        }else {
+            id=ids
+        }
+        def a=0
+        def b=0
+        for(def _id:id){
+            def cu=dataService.mongoDb.findOneCustomer([_id:_id])
+            if(cu._creatId==params._id||session.user.superUser){
+                dataService.mongoDb.delCustomer([_id:_id])
+                b++
+            }else {
+                a++
             }
-            '*'{ render status: NOT_FOUND }
+        }
+        if(a==0){
+            render(js(true,"全部成功"))
+        }else {
+            if(b!=0){
+                render(js(true,"部分成功，注意只有是你创建的用户才能被你或超级管理员删除！"))
+                return
+            }
+            render(js(false,"全部失败,注意只有是你创建的用户才能被你或超级管理员删除！！!"))
         }
     }
 }
--- web-app/vue/src/views/nav5/comein.vue
+++ web-app/vue/src/views/nav5/comein.vue
@@ -1,36 +1,22 @@
 <template>
-    <el-form ref="form" :model="form" :rules="rules" label-width="80px" @submit.prevent="onSubmit" style="100%" v-loading="loading">
-        <el-form-item label="  供应商" prop="name">
-            <el-input v-model="form.name" style="width: 50%;"></el-input>
+    <el-form :model="ruleForm" :rules="rules" v-loading="formLoading" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="选择供货商" prop="_Uid">
+            <el-select style="width: 300px" v-model="selUser" filterable remote @change="chu()" reserve-keyword placeholder="请输入姓名或者电话号码来查找"
+                       :remote-method="remoteMethod"
+                       clearable :loading="loading">
+                <el-option v-for="item in users" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+            </el-select>
+            <el-button type="primary" :disabled="disabled"  :loading="uloading"  @click="creatUU()">直接创建客户:{{uuuName}}</el-button>
         </el-form-item>
-        <el-form-item label="货物分类" prop="phone">
-            <el-input v-model="form.phone" style="width: 50%;"></el-input>
-        </el-form-item>
-        <el-form-item label="进货日期">
-            <el-input v-model="form.email" style="width: 50%;"></el-input>
-        </el-form-item>
-        <el-form-item label="付款金额">
-            <el-input v-model="form.qq" style="width: 50%;"></el-input>
-        </el-form-item>
-        <el-form-item label="付款状态">
-            <el-input v-model="form.qq" style="width: 50%;"></el-input>
-        </el-form-item>
-        <el-form-item label="付款日期">
-            <el-col :span="11">
-                <el-date-picker type="date" placeholder="选择日期" v-model="form.birth" style="width: 100%;"></el-date-picker>
-            </el-col>
-        </el-form-item>
-        <el-form-item label="备注">
-            <el-input type="textarea" v-model="form.remark"></el-input>
-        </el-form-item>
-        <el-form-item label="选择商品">
+        <el-form-item label="请选择商品">
             <el-select
                     v-model="value10"
                     multiple
                     filterable
                     :loading="gloading"
                     @change="change()"
-                    placeholder="选择商品">
+                    placeholder="请选择商品">
                 <el-option
                         v-for="item in options5"
                         :key="item.label"
@@ -85,9 +71,37 @@
                 </el-table-column>
             </el-table>
         </el-form-item>
+
+        <el-form-item label="交款方式" style="width:500px" prop="modeTransport">
+            <el-radio v-model="ruleForm.earnest" label="1">全款结清</el-radio>
+            <el-radio v-model="ruleForm.earnest" label="0">定金付款</el-radio>
+            <el-radio v-model="ruleForm.earnest" label="-1">赊账欠款</el-radio>
+        </el-form-item>
+        <!--<el-form-item label="支付方式" prop="payWay">-->
+        <!--<el-select v-model="ruleForm.payWay" placeholder="请选择支付方式" multiple>-->
+        <!--<el-option label="微信支付" value="微信支付"></el-option>-->
+        <!--<el-option label="支付宝支付" value="支付宝支付"></el-option>-->
+        <!--<el-option label="刷卡支付" value="刷卡支付"></el-option>-->
+        <!--<el-option label="现金支付" value="现金支付"></el-option>-->
+        <!--</el-select>-->
+        <!--</el-form-item>-->
+
+        <el-form-item label="交货时间" style="width:500px" prop="leadTime">
+            <el-date-picker v-model="ruleForm.leadTime" type="datetime" placeholder="选择日期时间">
+            </el-date-picker>
+        </el-form-item>
+        <el-form-item label="运输方式" style="width:500px" prop="modeTransport">
+            <el-input v-model="ruleForm.modeTransport"></el-input>
+        </el-form-item>
+        <el-form-item label="交货地址" prop="addr" style="width:500px">
+            <el-input type="textarea" v-model="ruleForm.addr"></el-input>
+        </el-form-item>
+        <el-form-item label="备注" prop="remark" style="width:500px">
+            <el-input type="textarea" v-model="ruleForm.remark"></el-input>
+        </el-form-item>
         <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button @click="cancel">取消</el-button>
+            <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+            <el-button @click="resetForm('ruleForm')">重置</el-button>
         </el-form-item>
     </el-form>
 </template>
@@ -102,39 +116,39 @@
                 disabled:true,
                 gloading:false,
                 uloading:false,
+                selUser:"",
+                user:"",
                 ruleForm: {
                     modeTransport : "",//运输方式
+                    phone:"",
                     _Uid: "",//客户id
-                    leadTime: " ",//交货时间
+                    leadTime:"",//交货时间
                     remark : "",//备注
                     amount :0,//合计金额
                     addr:"",//交货地址
                     num9:"",//数量
-                    earnest:0,
+                    earnest:"",
                 },
                 formLoading:false,
                 rules: {
                     _Uid: [
                         { required: true, message: '请输入用户', trigger: 'blur' },
                     ],
-                    region: [
-                        { required: true, message: '请选择活动区域', trigger: 'change' }
+                    modeTransport: [
+                        { required: true, message: '请输入运输方式', trigger: 'blur' }
                     ],
-                    date1: [
+                    leadTime: [
                         { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
                     ],
-                    date2: [
-                        { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+                    addr: [
+                        { required: true, message: '请输入送货地址', trigger: 'blur' }
                     ],
-                    type: [
-                        { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+                    earnest: [
+                        { required: true, message: '请选择交款方式', trigger: 'change' }
                     ],
-                    resource: [
-                        { required: true, message: '请选择活动资源', trigger: 'change' }
+                    phone: [
+                        { required: true, message: '请选输入联系电话', trigger: 'change' }
                     ],
-                    desc: [
-                        { required: true, message: '请填写活动形式', trigger: 'blur' }
-                    ]
                 },
                 users: [],
                 list: [],
@@ -151,38 +165,7 @@
                 }],
                 value10: [],
                 goodsTable:[],
-                form: {
-                    name: '',
-                    email : "",
-                    idCardNumber : "",
-                    phone : "",
-                    qq : " ",
-                    remark : "",
-                    sex:"",
-                    birth:"",
-                    addr:"",
-                },
-                rules:{
-                    name: [
-                        { required: true, message: '请输入姓名', trigger: 'blur' },
-                        { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' }
-                    ],
-                    email : "",
-                    idCardNumber : "",
-                    phone : [
-                        { required: true, message: '请输入电话', trigger: 'blur' },
-                        { min: 5, max: 15, message: '长度在 5 到 15 个字符', trigger: 'blur' }
-                    ],
-                    qq : " ",
-                    remark : "",
-                    sex:[
-                        { required: true, message: '请选择性别', trigger: 'change' }
-                    ],
-                    birth:"",
-                    addr:"",
-                },
-                loading:false,
-            }
+            };
         },
         methods: {
             getSummaries(param) {
@@ -225,18 +208,27 @@
                 this.ruleForm.amount=total
             },
             change(){
-                this.goodsTable=[]
+                let goodsTable=[]
                 this.value10.forEach(it=>{
                     let g=JSON.parse(it)
-                    g.amount=0
-                    this.goodsTable.push(g)
+                    for(let j = 0,len = this.goodsTable.length; j < len; j++){
+                        if(g._id==this.goodsTable[j]._id){
+                            g.num=this.goodsTable[j].num
+                            g.amount=this.goodsTable[j].amount
+                            break
+                        }
+                    }
+                    goodsTable.push(g)
                 })
+                this.goodsTable=goodsTable
             },
             getGoods(){
                 this.gloading=true
                 $.getJSON('api/product/getGoods',{name:this.name}).then(data=>{
                     this.options5=[]
                     data.list.forEach(item=>{
+                        item.num=0
+                        item.amount=0
                         let disabled
                         if(item.state==0)
                             disabled=false
@@ -250,36 +242,46 @@
             submitForm(formName) {
                 this.$refs[formName].validate((valid) => {
                     if (valid) {
-                        this.loading=true
-                        let form=JSON.parse(JSON.stringify(this.ruleForm))
-                        let detail=[]
-                        this.goodsTable.forEach(it=>{
-                            let a={
-                                _id:it._id,
-                                name:it.name,
-                                code:it.code,
-                                num:it.num,
-                                price:it.price,
-                            }
-                            detail.push(a)
-                        })
-                        form.detail=JSON.stringify(detail)
-                        form.leadTime = (!this.ruleForm.leadTime || this.ruleForm.leadTime == '') ? '' : util.formatDate.format(new Date(this.ruleForm.leadTime), 'yyyy-MM-dd');
-                        $.getJSON('api/order/creat',form).then(data=>{
-                            if(data.flag){
-                                this.$notify({
-                                    title: '成功',
-                                    message: data.remark,
-                                    type: 'success'
-                                });
-                                this.$refs['ruleForm'].resetFields();
-                            }else {
-                                this.$message({
-                                    message: data.remark,
-                                    type: 'error'
-                                });
-                            }
-                            this.loading=false
+                        if(this.goodsTable.length==0){
+                            this.$message({
+                                message:'请至少选择一个商品！',
+                                type: 'error'
+                            });
+                            return false;
+                        }
+                        this.$confirm('确认提交吗？', '提示', {}).then(() => {
+                            this.loading=true
+                            let form=JSON.parse(JSON.stringify(this.ruleForm))
+                            let detail=[]
+                            this.goodsTable.forEach(it=>{
+                                let a={
+                                    _id:it._id,
+                                    name:it.name,
+                                    code:it.code,
+                                    num:it.num,
+                                    price:it.price,
+                                    total:it.amount,
+                                }
+                                detail.push(a)
+                            })
+                            form.detail=JSON.stringify(detail)
+                            form.leadTime = (!this.ruleForm.leadTime || this.ruleForm.leadTime == '') ? '' : util.formatDate.format(new Date(this.ruleForm.leadTime), 'yyyy-MM-dd hh:mm:ss');
+                            $.getJSON('api/order/creat',form).then(data=>{
+                                if(data.flag){
+                                    this.$notify({
+                                        title: '成功',
+                                        message: data.remark,
+                                        type: 'success'
+                                    });
+                                    this.$refs['ruleForm'].resetFields();
+                                }else {
+                                    this.$message({
+                                        message: data.remark,
+                                        type: 'error'
+                                    });
+                                }
+                                this.loading=false
+                            })
                         })
                     } else {
                         this.$message({
@@ -296,7 +298,7 @@
             remoteMethod(query) {
                 this.loading=true
                 if (query !== '') {
-                    $.getJSON('api/customer/getUsers',{name:query}).then(data=>{
+                    $.getJSON('api/stock/getUsers',{name:query}).then(data=>{
                         if(data.flag){
                             this.users=data.users
                             if(this.users.length>=1){
@@ -319,13 +321,17 @@
                 }
             },
             creatUU(){
+                if(this.ruleForm._Uid){
+                    this.disabled=true
+                    return
+                }
                 this.uloading=true
                 let form={
                     name:this.uuuName,
                     phone:this.uuuName,
                     sex:"男",
                 }
-                $.getJSON('api/customer/creat',form).then(data=>{
+                $.getJSON('api/stock/creat',form).then(data=>{
                     if(data.flag){
                         this.$notify({
                             title: '创建用户成功',
@@ -343,55 +349,18 @@
                     this.remoteMethod(this.uuuName)
                 })
             },
-
+            chu(){
+                if(this.selUser){
+                    this.user=JSON.parse(this.selUser)
+                }else {
+                    this.user={}
+                }
+                this.ruleForm._Uid=this.user._id
+                this.ruleForm.phone=this.user.phone
+            }
+        },
         mounted() {
             this.getGoods()
-        },
-            onSubmit() {
-                this.$refs['form'].validate((valid) => {
-                    if (valid) {
-                        this.loading=true
-                        this.form.birth = (!this.form.birth || this.form.birth == '') ? '' : util.formatDate.format(new Date(this.form.birth), 'yyyy-MM-dd');
-                        $.getJSON('api/customer/creat',this.form).then(data=>{
-                            if(data.flag){
-                                this.$notify({
-                                    title: '成功',
-                                    message: data.remark,
-                                    type: 'success'
-                                });
-                                this.form= {
-                                    name: '',
-                                    email : "",
-                                    idCardNumber : "",
-                                    phone : "",
-                                    qq : " ",
-                                    remark : "",
-                                    sex:"",
-                                    birth:"",
-                                    addr:"",
-                                }
-                                this.$refs['form'].resetFields();
-                                this.loading=false
-                            }else {
-                                this.$message({
-                                    message: data.remark,
-                                    type: 'error'
-                                });
-                            }
-                        })
-                    } else {
-                        this.$message({
-                            message:'请填写必要数据！',
-                            type: 'error'
-                        });
-                        return false;
-                    }
-                });
-            },
-            cancel(){
-                this.$router.push({ path: '/table' });
-            }
         }
     }
-
 </script>
