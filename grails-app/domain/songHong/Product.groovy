b6b041dee0cc88d9db8ec39e8ef322ddc8085034
孙尧明 <906192423@qq.com>
2019-04-11 22:01:26 +0800

    6.2 订单显示

--- grails-app/domain/songHong/Product.groovy
+++ grails-app/domain/songHong/Product.groovy
@@ -14,6 +14,13 @@ class Product {
                 number:"",//库存数量
                 price:0,//单价
                 unit:"",//单位
+                group:false,//是否为组合商品
+                count:[
+//                        [
+//                               _id:"",//商品_id 标识
+//                               number:"",//需要数量
+//                                ]
+                ],
                 ct  : new Date().format("yyyy-MM-dd HH:mm:ss.SSS"),//创建时间
                 state: 0,//0为可生产，-1无法生产
 
