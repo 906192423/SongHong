3725acc6fc614897055a86c429ea2f7d9caf54bb
宋洪锋 <hong19970529@qq.com>
2019-04-11 21:53:33 +0800

    组合商品

---
+++ grails-app/domain/songHong/Exchange.groovy
@@ -0,0 +1,39 @@
+package songHong
+
+class Exchange {
+
+    static constraints = {
+    }
+    static newOne = { initObj = null ->
+        def item = [
+                _id : "",
+                _creatId:"",//创建者id
+                creatName:"",//创建者姓名
+                code: "",//订单号
+                detail:[
+//                        [
+//                                _id: "",
+//                                name:"",
+//                                code: "",
+//                                num:0,
+//                                price:0,
+//                        ],
+                ],
+                cashList:[
+//                        [
+//                                _id:"",
+//                                code:"",
+//                                amount:"",
+//                        ]
+                ],//交易单号
+                userName : "",//客户姓名
+                _Uid: "",//客户id
+                remark : "",//备注
+                amount :"0",//合计金额
+                ct  : new Date().format("yyyy-MM-dd HH:mm:ss.SSS"),//创建时间
+                allPay:false,//false未付，true全部付清
+        ]
+        if (initObj) item.putAll(initObj)
+        return item
+    }
+}
