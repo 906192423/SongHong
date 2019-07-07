4ea5bf70eb732fda8265fc3827cfa41eb3ccfe0f
宋洪锋 <hong19970529@qq.com>
2019-07-07 09:10:42 +0800

    打印样式5.0

--- web-app/vue/src/views/nav4/payList.vue
+++ web-app/vue/src/views/nav4/payList.vue
@@ -72,12 +72,12 @@
             </el-table-column>
             <el-table-column prop="creatName" label="创建人" width="120" sortable>
             </el-table-column>
-            <el-table-column label="操作" width="200">
-                <template slot-scope="scope">
-                    <el-button size="small" @click="handleEdit(scope.row)">查看详细</el-button>
-                    <el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
-                </template>
-            </el-table-column>
+            <!--<el-table-column label="操作" width="200">-->
+                <!--<template slot-scope="scope">-->
+                    <!--<el-button size="small" @click="handleEdit(scope.row)">查看详细</el-button>-->
+                    <!--<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>-->
+                <!--</template>-->
+            <!--</el-table-column>-->
         </el-table>
 
         <!--工具条-->
